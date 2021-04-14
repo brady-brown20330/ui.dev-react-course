35fbe3a1efd5c2355bf56614b1041acf903fe408
Brady Brown <brownb202@gmail.com>
2021-04-13 20:57:56 -0600

    app now passes playerOne and playerTwo as part of the url query string instead of in props

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme';
+import Results from './components/Results'
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
@@ -28,7 +29,8 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
               <Route exact path='/' component={Popular} />
-              <Route path='/battle' component={Battle} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/battle/results' component={Results} />
             </div>
           </div>
         </ThemeProvider>
